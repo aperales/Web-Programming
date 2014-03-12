f04c057bddfc79b404f702406a70cd1cba3169ca
Albert Perales <aperales@fullsail.com>
2014-03-12 17:47:00 -0600

    arithmetic operators

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,8 +1,8 @@
+//expressions - Arithmetic Operators
 
+//are of the triangle is half of the width times the height
 
-var yearBorn = 1982;
-
-var age = 2014 - yearBorn - 1;
-
-console.log(age);
-
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area)
