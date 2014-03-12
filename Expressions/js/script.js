00803c13e77a2868abd74ec1069180e6f7e94b3e
Albert Perales <aperales@fullsail.com>
2014-03-12 17:51:40 -0600

    MOdulo

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,8 +1,4 @@
-//expressions - Arithmetic Operators
+//Expressions - Modulo
 
-//are of the triangle is half of the width times the height
-
-var width = 4;
-var height = 5;
-var area = width * height/2;
-console.log(area)
+var remainder = 9 % 2;
+console.log(remainder)
