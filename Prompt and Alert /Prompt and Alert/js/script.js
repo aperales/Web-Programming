aef6723a4d74171790fd76a70388e490e768ff0e
Albert Perales <aperales@fullsail.com>
2014-03-13 01:30:35 -0600

    Prompt and Alert

---
+++ Prompt and Alert /Prompt and Alert/js/script.js
@@ -0,0 +1,10 @@
+
+//Prompt and Alert
+
+var width = prompt("We are calculating the are of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+
+var area = width * height;//calculates info
+//console.log(area); //prints to console
+
+alert("The area of your rectangle is " + area + " square feet");
