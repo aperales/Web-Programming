0c60212bcfa40ce2e1bdf585ebd94570630f1a97
Albert Perales <aperales@fullsail.com>
2014-03-13 13:52:03 -0600

    Comments on each code line

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,11 +6,18 @@
 //figure out how many customers we have for the week and what the average per day was.
 
 
-
+//Array with daily customers for 1 week. You can input any number to change answer.
 var custPerDay = [65, 44, 48, 78, 89, 117, 57];
+// variable of days in a week
 var days = 7;
+// expression for total weekly customers using array.
 var total = custPerDay[0] + custPerDay[1] + custPerDay[2] + custPerDay[3] + custPerDay[4] + custPerDay[5] + custPerDay[6];
+//expression to find average using total days and array information total
 var averagePerDay = total / days;
+
+// console log check with sentence links for correct answer
+//you have to have the quotations and parenthesis to separate links and total that console is checking for
+
 
 console.log(("There was ") + (total) + (" customers for the week."));
 console.log(("The average number of customers per day was ") + (averagePerDay) + ("."));
