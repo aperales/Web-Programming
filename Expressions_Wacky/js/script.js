52ceacba78eff02325be61a6f1d42b321f4b1c09
Albert Perales <aperales@fullsail.com>
2014-03-13 14:59:15 -0600

    variables set wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,4 +3,10 @@
 // A factory employs 98 workers.  Out of these workers, 16 receive a wage of $85 per day
 // and the rest receive $120.75 per day.  A week is 5 working days. How much does the
 //factory pay each week in wages.
-
+var pay1 = 120.75;
+var pay2 = 85;
+var totalEmp = 98;
+var employee1 = 16;
+var employee2 = totalEmp - employee1;
+//Total employee 2
+console.log(employee2);
