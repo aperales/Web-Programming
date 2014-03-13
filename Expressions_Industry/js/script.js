a75ef7fa9ff2e177ebcecf93739a68a1c17f9081
Albert Perales <aperales@fullsail.com>
2014-03-13 13:26:20 -0600

    Expression with array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,15 @@
-// Alberto Perales 3/13/2014 Expression_Industry 
+// Alberto Perales 3/13/2014 Expression_Industry
 
 //In My job at T-Mobile we have a different amount of customers a day.
 //On Monday we have 65, Tuesday we have 44, Wednesday we have 48, Thursday we have 78
-//Friday 89, Saturday 117, and Sunday we have 57.  HWat is the total amount
-// of customers for the week? What is the average customers per day?
+//Friday 89, Saturday 117, and Sunday we have 57.  Every sunday night I have to
+//figure out how many customers we have for the week and what the average per day was.
+
+
+
+var custPerDay = [65, 44, 48, 78, 89, 117, 57];
+
+var total = custPerDay[0] + custPerDay[1] + custPerDay[2] + custPerDay[3] + custPerDay[4] + custPerDay[5] + custPerDay[6];
+
+console.log(total);
+
