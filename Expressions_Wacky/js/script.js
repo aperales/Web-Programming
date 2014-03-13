a5e25832bbb3e329196089cd239f6543c24e23f6
Albert Perales <aperales@fullsail.com>
2014-03-13 15:24:42 -0600

    comments on lines of code

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,12 +2,15 @@
 
 // A factory employs 98 workers.  Out of these workers, 16 receive a wage of $85 per day
 // and the rest receive $120.75 per day.  A week is 5 working days. How much does the
-//factory pay each week in wages.
+//
+//Variables for given in problem created // variables can change to any number // pay
 var pay1 = 120.75;
 var pay2 = 85;
+//Employee variables for givens //total employees and expression to find second set of employees
 var totalEmp = 98;
 var employee1 = 16;
 var employee2 = totalEmp - employee1;
+//Weekdays given per working week on problem
 var weekDays = 5
 //Order of Operations. The expression will need to have parenthesis for correct answer to expression
 //Expression will need week days in each part of parenthesis
