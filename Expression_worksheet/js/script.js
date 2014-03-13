6fdae0c3c04730550adafbd936282ace835c3376
Albert Perales <aperales@fullsail.com>
2014-03-13 00:02:08 -0600

    Slice of pie 2

--- Expression_worksheet/js/script.js
+++ Expression_worksheet/js/script.js
@@ -13,9 +13,13 @@ var humanYears = dogYears * 7;
 
 //Expression to find human year
 dogYears * 7;
-console.log(humanYears);
+
+
 
 //Sparky is 21 human years old which is 3 in dog years
+console.log(humanYears);
+
+
 
 //Slice of Pie part 1
 // given variables
@@ -29,8 +33,28 @@ var pizzasOrd = 4;
 // expression to find slices per person
 var totalSlices = pizzasOrd * slicesPerPizza / person;
 
-
+//Pizza slices per person 4.5
 console.log(totalSlices);
+
+
+//Slice of Pie part 2
+
+var person = 7;
+var slices = 32;
+var slicesPerson = slices / person;
+var leftPizzaPerson = .5
+var totalPizzaLeft = leftPizzaPerson * person
+
+
+//Pizzas per person 4.5
+//half pizza slice per person
+
+
+
+
+//Sparky got 3.5 slices of pizza
+console.log(totalPizzaLeft);
+
 
 
 
