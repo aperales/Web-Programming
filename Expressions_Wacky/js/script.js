08192fb2097fefa65bb07d959680aec86b8e46ad
Albert Perales <aperales@fullsail.com>
2014-03-13 14:19:04 -0600

    Problem for wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,6 @@
 //Alberto Perales 3/13/2014 Expressions_Wacky
+
+// A factory employs 98 workers.  Out of these workers, 16 receive a wage of $85 per day
+// and the rest receive $120.75 per day.  A week is 5 working days. How much does the
+//factory pay each week in wages.
+
