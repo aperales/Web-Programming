c681ebd69b51c9ae75b187daa70d2329cbc623f0
Albert Perales <aperales@fullsail.com>
2014-03-12 23:17:08 -0600

    Dog Years

--- Expression_worksheet/js/script.js
+++ Expression_worksheet/js/script.js
@@ -1,3 +1,18 @@
 /**
  * Created by selarepal24 on 3/12/14.
  */
+//Alberto Perales 3/12/2014 Expressions Worksheet
+
+//Calculate Sparky's Dog Years
+
+//Sparky's Age
+var dogYears = 3;
+
+//Find human years per dog years
+var humanYears = dogYears * 7;
+
+//Expression to find human year
+dogYears * 7;
+console.log(humanYears);
+
+//Sparky is 21 human years old which is 3 in dog years
