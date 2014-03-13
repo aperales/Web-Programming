2b28180cca748b7e0c29729dffd8ee2c042203e0
Albert Perales <aperales@fullsail.com>
2014-03-13 12:46:54 -0600

    Expressions_Personal problem submitted

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,26 @@
 // ALberto Perales 3/13/2014 Expressions_Wacky
 
+// There were 10 people at breakfast.  I had to buy donuts and coffee for everyone.
+//Each person should get an equal amount of donuts and one coffee.
+// How many donuts did each person get, and how many coffees did I buy?
 
+// Variables for givens
+
+var coffee = 1
+var people = 10;
+var donutsPerDoz = 12
+var dozenBought= 3
+
+//Expressions to find amounts
+var totalDonuts = dozenBought * donutsPerDoz;
+var donutsPerPers = totalDonuts / people;
+var coffeePerPers = coffee * people;
+
+// Result Variable
+
+// NUmber of donuts per person
+// Console for dopnuts
+console.log(donutsPerPers + (" donuts per person"));
+
+// console result for coffee per person
+console.log(coffeePerPers + (" coffee needed"));
