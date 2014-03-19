880b8e903cd337fe7cebeb48788d016ae1832964
Albert Perales <aperales@fullsail.com>
2014-03-19 00:27:26 -0600

    Logical Operators

--- Conditional Statements/js/script.js
+++ Conditional Statements/js/script.js
@@ -1,20 +1,14 @@
-//Conditional Logic - else if
+//Conditional Logic - Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of kid with the parent
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console " you can ride"
-//if kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //YOu can ride
-    console.log("You can ride the coaster");
-}else if(kidHeight > wParentHeight){
-    //You can ride with parent present
-    console.log("You can ride, but only with a parent present.");
-
+//if price of phone is less than budget and if paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+    console.log("WE can buy the phone");
 }else{
-    //Sorry you have growing to
-    cosole.log("Sorry kid, you have some growing to do first!");
-
+    console.log("No phone for you!!");
 }
+
+
