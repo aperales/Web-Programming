9c9eed7b35e41f7efd530fffe6593443ec02643d
Albert Perales <aperales@fullsail.com>
2014-03-19 00:00:15 -0600

    else if

--- Conditional Statements/js/script.js
+++ Conditional Statements/js/script.js
@@ -1,18 +1,20 @@
-//Conditional Logic - WIth Expression
+//Conditional Logic - else if
 
-var kidHeight = 43;
+var kidHeight = 52;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of kid with the parent
 
 //if the child is old enough, print to the console " you can ride"
 //if kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //YOu can ride
     console.log("You can ride the coaster");
-
+}else if(kidHeight > wParentHeight){
+    //You can ride with parent present
+    console.log("You can ride, but only with a parent present.");
 
 }else{
-    //code performed if condition is false
-    console.log("Sorry kid, you have some growing to do first!")
+    //Sorry you have growing to
+    cosole.log("Sorry kid, you have some growing to do first!");
 
 }
