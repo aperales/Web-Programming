a69666506417ae3236623cfaa9cf6bf613d5a81c
Albert Perales <aperales@fullsail.com>
2014-03-18 23:33:21 -0600

    conditional 1st screencast

---
+++ Conditional Statements/js/script.js
@@ -0,0 +1,13 @@
+//Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to console "you can ride!"
+// condition inside parenthesis
+if(oldEnough){
+    // code performed if condition true
+    console.log("You can ride the coaster")
+
+}
+
+
