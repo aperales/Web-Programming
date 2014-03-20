a80429c7a2df72e9afe5880a52b39257713dda77
Albert Perales <aperales@fullsail.com>
2014-03-20 17:18:34 -0600

    added alerts for if else

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,29 +2,33 @@
 
 //if my daughter goes online to a website and the age restriction is 18 years of age.
 //she was born in 2005.  Will the website allow access to it. If she asks me
-//to grant her access and I wwas born in 1982.  how old is she and will it grant me access
+//to grant her access and I wwas born in 1982.  how old is she and will it grant her access?
 
 var restriction = 18;
 //yearBorn = 2005;
 
+//prompt to start entering inforamtion and age limit for access
+
 var dob = prompt("TO ACCESS \nYou need to be at least 18yrs old\nPlease Enter year of birth.");
 
+//var prompt to enter present year
 var presentYear = prompt("Please enter present year");
+//alert to thank that the infomration was entered
 
+alert("Thank you for the information.");
+//variable to find age expression
 var age = presentYear - dob;
-console.log(("Her age is ") + age + ("."));
+//console print to find age
+console.log(("Age is ") + age + ("."));
 alert(("Age is ") + age);
-alert("Access not granted! ");
-
-
 
 
 if(age < restriction){
-    console.log("Access Denied");
-
+    console.log("Your access is Denied");
+    alert("Access denied")
 }else{
-    console.log("Access granted!")
-
+    console.log("Access granted!");
+    alert("Access granted");
 }
 
 
