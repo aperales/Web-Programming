d0e6abe591f0bcfd1c567ae51bc4193c33ef3867
Albert Perales <aperales@fullsail.com>
2014-03-20 17:48:58 -0600

    problem created for Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by selarepal24 on 3/19/14.
- */
+//Alberto Perales 3/20/2014 Conditions_Industry
+
+//on average 1 out of 5 web sites created fails because of improper planning.
+//HOw many websites have you created? enter the information to find out
+// how many have failed for you.
+
+alert("FAILING WEBSITES\nUsing a national average.");
+var websites = prompt("Please enter websites created");
+
+var createdWeb = websites / 5;
+
+
+console.log(createdWeb);
