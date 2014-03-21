a6f74d69ca8c26d623599160835c1ca899ec8cc8
Albert Perales <aperales@fullsail.com>
2014-03-20 18:04:57 -0600

    problem for industry created

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,11 +3,22 @@
 //on average 1 out of 5 web sites created fails because of improper planning.
 //HOw many websites have you created? enter the information to find out
 // how many have failed for you.
-
+//The average is 5 to 10 fails per designer per company.  Are you within average?
+var averageCom1 = 5;
+var averageCom2 = 10;
 alert("FAILING WEBSITES\nUsing a national average.");
 var websites = prompt("Please enter websites created");
 
 var createdWeb = websites / 5;
+console.log((createdWeb) + (" websites have failed on average."));
+
+if(createdWeb >= averageCom1 && createdWeb <= averageCom2){
+    console.log("You are at average")
 
 
-console.log(createdWeb);
+}else{
+    console.log("You are not within average")
+
+}
+
+
