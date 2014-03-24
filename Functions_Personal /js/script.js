620525b39e3a18d8bab2b393294ffa91044e3f90
Albert Perales <aperales@fullsail.com>
2014-03-24 17:43:47 -0600

    Functions personal problem in comments

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -1,3 +1,15 @@
-/**
- * Created by selarepal24 on 3/24/14.
- */
+//Alberto Perales 3/24/2014 Functions_Personal
+
+//I bought 3 sets of colored pencils for a project.
+//Each box of color pencils has 24 colors.  My daughter was using them
+//to color and she lost 7 pencils.  HOw many colored pencils do I have now. I need 85
+// total for a project do i have 85?
+
+alert("We are calculating how many colored pencils we have.\nPlease enter the following information.");
+var boxes = prompt("Please enter the number of boxes bought.");
+var perBox = prompt("please enter the number of pencils per box.");
+var totalPencils = boxes * perBox;
+console.log(totalPencils);
+alert("The total number of colored pencils is " + totalPencils);
+
+
