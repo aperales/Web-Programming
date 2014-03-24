749cb56c96be89f56a409e0dec9144bcf9849832
Albert Perales <aperales@fullsail.com>
2014-03-24 15:49:42 -0600

    comments for problem 1 finished test

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -5,17 +5,22 @@
 
 //calculae circumference of circle
 
-
-function calculateCirc(){
+//Function for calculating the circumference of a circle
+function calcCirc(){
+    //variables given to find the circum of circel using formula
+    // 2 * pi * r = circum
     var pi = 3.14;
     var radius = 3;
     var circlePart = 2;
     var circumference = circlePart * pi * radius;
+    //consle print to check answer
     console.log (circumference);
-
+    //console print for answer needed
+    console.log("The circumference of the circle is " + (circumference));
 
 }
-
+//invoke function
+calcCirc();
 
 
 
