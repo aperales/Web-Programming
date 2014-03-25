a07bb4e78ea8097b2af4b1f53dc23fe830ea9aa9
Albert Perales <aperales@fullsail.com>
2014-03-24 18:26:18 -0600

    Finished testing code for functions personal

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -3,12 +3,13 @@
 //I bought 3 sets of colored pencils for a project.
 //Each box of color pencils has 24 colors.  My daughter was using them
 //to color and she lost 7 pencils.  HOw many colored pencils do I have now. I need 85
-// total for a project do i have 85?
+// total for a project do i have 85, but not more than 100?
 
 alert("We are calculating how many colored pencils we have.\nPlease enter the following information.");
 var boxes = prompt("Please enter the number of boxes bought.");
 var perBox = prompt("please enter the number of pencils per box.");
 var pencilsNeeded = 85;
+var over = 100;
 
 
 
@@ -18,13 +19,19 @@ function calcPencils(){
 var totalPencils = boxes * perBox - 7;
     console.log(totalPencils);
     alert("The total number of colored pencils is " + totalPencils);
-    if(totalPencils > pencilsNeeded){
-        console.log("You have enough pencils")
+    if(totalPencils > pencilsNeeded && totalPencils < over){
+        console.log("You have enough pencils");
         alert("You have enough pencils!")
+}else if(totalPencils > over){
+
+        console.log("You are over 100 pencils");
+        alert("You are over 100 pencils");
+
 }else{
         console.log("You do not have enough pencils!");
-        alert("You do not have enough pencils!");
-}
+        alert("You don't have enough pencils")
+    }
+alert("Thank you for the information");
 
 }
 calcPencils();
