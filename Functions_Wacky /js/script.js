5d05d2677290584d042ac772930aae6fe4ca1b91
Albert Perales <aperales@fullsail.com>
2014-03-26 15:00:04 -0600

    Frist problem started for wacky

--- Functions_Wacky /js/script.js
+++ Functions_Wacky /js/script.js
@@ -1,3 +1,7 @@
 //Alberto Perales 3/26/2014 Functions_Wacky
 
 
+//I made 3 liters of tea on monday, on tuesday I made 4 liters and on friday I made
+//7 liters.  Larry drank 2 litters on monday, 4 on tuesday and 4 on friday.
+// How many liters did I make in all three days and how many did larry drink and
+//what is left?
