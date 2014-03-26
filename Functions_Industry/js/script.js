be77c6a9264085b955f7ecd0f7007bda90e23f14
Albert Perales <aperales@fullsail.com>
2014-03-26 16:54:22 -0600

    problem finished will continue to edit

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,6 +3,15 @@
 //it takes a a web designer 25 tries to get a web site correct.
 //How many tries does it take to make 3 websites.
 
-var tries = 25;
-var website = 3;
+alert("To find the tries it take to get a website correct.\nPlease enter following info:");
+var tries = prompt("Please enter the tries.");
+var websites = prompt("Please enter number of websites created.");
 
+
+function totalTries(){
+var total = tries * websites;
+console.log("It takes " + total + " tries to create " + websites + " websites.");
+
+
+}
+totalTries();
