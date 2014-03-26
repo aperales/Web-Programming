75c4079210a15c5e72874de4a776bf9e1d4818a6
Albert Perales <aperales@fullsail.com>
2014-03-26 16:16:19 -0600

    comments made for each line of code

--- Functions_Wacky /js/script.js
+++ Functions_Wacky /js/script.js
@@ -7,25 +7,37 @@
 
 
 
-
+//variable given by user and from the problem
+// total tea made
 var totalTea = prompt("We need to find out how much tea was made.\nPlease enter Tea made per day.");
+//var for days tea was mde
 var days = 3;
+//tea liters per day made
 var teaPerDay = 3;
+//var expression to find out tea made for the 3 days
 var teaMade = totalTea * teaPerDay;
+//console print of answer
 console.log(teaMade);
+//alert for user of tea made total
 alert("Total tea made was " + teaMade + " liters per day.");
+//var to find out how much tea larry drinks
 var larryTea = prompt("Enter how much Larry drinks per day");
+//var expression to fin dout how much tea larry drinks
 var totalLarryTea = larryTea * days;
+//console print total larry tea
 console.log(totalLarryTea);
+//alert for user how much tea larry drinks total
 alert("Larry drinks " + totalLarryTea + " liters of tea.");
 //Team made was 12l per 3 days larry drinks 6l per 3 days
+//var for function to find how much tea is left form tea made and waht larry drinks
 var total = calcteaLeft(12, 6);
 function calcteaLeft(t, l){
     var teaLeft = teaMade - totalLarryTea;
     return(teaLeft);
 
 }
-console.log(total);
+//console print for how much tea is left after larry drinks 9 form the 12 liters
+console.log(total + " liters of tea left.");
 alert("There is " + total + " liters of tea left.");
 calcteaLeft();
 
