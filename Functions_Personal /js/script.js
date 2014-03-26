0b55da52567b5fb52c3719e7d5895ef69598684b
Albert Perales <aperales@fullsail.com>
2014-03-26 14:46:50 -0600

    final test and comments on personal function s

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -4,34 +4,36 @@
 //Each box of color pencils has 24 colors.  My daughter was using them
 //to color and she lost 7 pencils.  HOw many colored pencils do I have now. I need 85
 // total for a project do i have 85, but not more than 100?
-
+// alert user what the problem is and what to input
 alert("We are calculating how many colored pencils we have.\nPlease enter the following information.");
+//variables given in problem with prompt to enter information
 var boxes = prompt("Please enter the number of boxes bought.");
 var perBox = prompt("please enter the number of pencils per box.");
+//variables given in problem no prompt
 var pencilsNeeded = 85;
 var over = 100;
 
 
 
-
+//funstion to cal pencils
 function calcPencils(){
-
+//variable to find total pencils using givens from about and information user entering
 var totalPencils = boxes * perBox - 7;
-    console.log(totalPencils);
-    alert("The total number of colored pencils is " + totalPencils);
-    if(totalPencils > pencilsNeeded && totalPencils < over){
-        console.log("You have enough pencils");
-        alert("You have enough pencils!")
-}else if(totalPencils > over){
+    console.log(totalPencils);//log print total pencils
+    alert("The total number of colored pencils is " + totalPencils);//alert user toatl pencils
+    if(totalPencils > pencilsNeeded && totalPencils < over){//if to print and alert user if you have enbough poencils
+        console.log("You have enough pencils");//console print enough pencils
+        alert("You have enough pencils!")//alert to user enough pencils
+}else if(totalPencils > over){//else if for user going over 100 pencils like on problem
 
-        console.log("You are over 100 pencils");
-        alert("You are over 100 pencils");
+        console.log("You are over 100 pencils");//console print ove rpencils
+        alert("You are over 100 pencils");//alert over poenceils to user
 
-}else{
+}else{//else if console print and alert you don ot have enough pencils
         console.log("You do not have enough pencils!");
         alert("You don't have enough pencils")
     }
-alert("Thank you for the information");
+alert("Thank you for the information");//thatnk you alert for user
 
 }
 calcPencils();
