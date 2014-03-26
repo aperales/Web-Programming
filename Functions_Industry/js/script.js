bb97c989210e02cf67a8d9f7218f51f910078a22
Albert Perales <aperales@fullsail.com>
2014-03-26 16:38:54 -0600

    Industry problem submitted

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,8 @@
 //Alberto Perales 3/26/2014 Functions Industry
 
+//it takes a a web designer 25 tries to get a web site correct.
+//How many tries does it take to make 3 websites.
+
+var tries = 25;
+var website = 3;
 
